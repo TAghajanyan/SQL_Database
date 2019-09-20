05ff2d36b3a80cad6ac242423a924e7b1ca0803b
TAghajanyan <taghajanyan99@gmail.com>
2019-09-20 12:13:43 +0400

    Create Creating DB \ Table.sql

---
+++ SQL_Starter/Creating DB \ Table.sql
@@ -0,0 +1,30 @@
+Create database School
+
+USE School
+
+CREATE TABLE Students
+(
+    ID int primary key NOT NULL IDENTITY(1,5000),
+    FirstName varchar(5),
+    LastName varchar(10),
+	  Birthday date,
+	  Gender varchar(10)
+); 
+
+alter table Students
+alter column FirstName nvarchar(20) not null
+
+alter table Students
+alter column LastName nvarchar(20) not null
+
+alter table Students
+add Phone varchar(13) unique not null
+
+alter table Students
+add EMail varchar(20) unique not null
+
+alter table Students
+add Country varchar(30) not null
+
+alter table Students
+add GPA real not null
